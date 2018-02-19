20923d82e94c78c84161d36f71e82211c2945d37
Sujata <sujatakokane>
2018-02-19 09:11:45 -0800

    updated

---
+++ src/main/java/com/sanera/telecom/profiler/ProfilerApplication.java
@@ -0,0 +1,12 @@
+package com.sanera.telecom.profiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilerApplication.class, args);
+	}
+}
