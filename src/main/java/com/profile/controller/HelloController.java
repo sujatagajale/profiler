6d3de89857711f9f7843110d6116f6c9dc9992c4
sujatagajale <36452348+sujatagajale@users.noreply.github.com>
2018-02-28 07:10:58 -0800

    Added logger

--- src/main/java/com/profile/controller/HelloController.java
+++ src/main/java/com/profile/controller/HelloController.java
@@ -3,12 +3,20 @@ package com.profile.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Controller
 public class HelloController {
 
+	private static final Logger logger = LogManager.getLogger(HelloController.class);
+
 	@GetMapping("/hello")
 	public String hello(Model model) {
+		logger.trace("Entering application...");
+
+		logger.info("Hello Log4j2...");
+		
 
 		model.addAttribute("name", "Sanjay Kokane");
 
